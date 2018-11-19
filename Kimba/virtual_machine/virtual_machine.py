c679f06131279558db65958fe78daf5b7e38581b
eduardocd12 <eduardocd1203@gmail.com>
2018-11-18 23:32:52 -0600

    Funcion run en Virtual Machine

--- Kimba/virtual_machine/virtual_machine.py
+++ Kimba/virtual_machine/virtual_machine.py
@@ -1,5 +1,7 @@
 from assets.function_directory import FunctionDirectory
+from virtual_machine.memory_request import MemoryRequest
 
+import turtle
 
 class VirtualMachine():
     def __init__(self, memory, function_directory, instructions):
@@ -7,4 +9,15 @@ class VirtualMachine():
         self.function_directory = function_directory
         self.instructions = instructions
         self.number_of_instructions = len(self.instructions)
-        self.number_of_current_instruction = 0
+        self.instruction_counter = 0
+
+    def run(self):
+    	current_function = {}
+    	parameter_position = 0
+    	current_memory = self.memory
+    	while self.instruction_counter < self.number_of_instructions:
+            current_instruction = self.instructions[self.instruction_counter]
+            operator = current_instruction.operator
+            left_operand_address = current_instruction.left_operand
+            right_operand_address = current_instruction.right_operand
+            result_address = current_instruction.result
